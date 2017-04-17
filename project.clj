3808965bfd060cea0b341fa61300b38b2662d9c8
Saul Shanabrook <s.shanabrook@gmail.com>
2017-04-17 08:46:51 -0400

    make snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-check-stdin "0.2.0"
+(defproject lein-check-stdin "0.3.0-SNAPSHOT"
   :description "Prints errors encountered when reading stdin as clojure code"
   :url "http://example.com/saulshanabrook/lein-check-stdin"
   :license {:name "Eclipse Public License"
@@ -6,7 +6,9 @@
   :eval-in-leiningen true
   :dependencies [[org.clojure/data.json "0.2.6"]]
   :deploy-repositories [["releases" {:url "https://clojars.org/repo"
-                                     :sign-releases false}]]
+                                     :username :env
+                                     :sign-releases false
+                                     :password :env}]]
   :release-tasks [["vcs" "assert-committed"]
                   ["change" "version" "leiningen.release/bump-version" "release"]
                   ["vcs" "commit"]
